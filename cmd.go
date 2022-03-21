8bc6c93442aaa99d8c0d99f5b9e4e57e0d8658af
sdkjwjq <695292202@qq.com>
2022-03-21 18:01:10 +0800

    menu 1.0“

---
+++ cmd.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for {
+		var cmd string
+		fmt.Scan(&cmd)
+		switch cmd {
+		case "help":
+			fmt.Println("*This is a help cmd! You can enter the following command.")
+			fmt.Println("*help\tTo get some help about commands.")
+			fmt.Println("*version\tGet the current version of the menu.")
+			fmt.Println("*quit\tExit the menu.")
+
+		case "version":
+			fmt.Println("This version is 1.0")
+
+		case "quit":
+			return
+		default:
+			fmt.Println("It's a wrong cmd")
+		}
+
+	}
+}
